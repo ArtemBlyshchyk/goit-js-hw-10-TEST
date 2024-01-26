8408157a621f1962738e71bc8afd3030d7d61dcd
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-01-26 22:02:11 +0200

    Solved first task

---
+++ src/js/1-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+
+const inputText = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]');
+
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate < new Date()) {
+      //   window.alert('Please choose a date in the future');
+      iziToast.warning({
+        message: 'Please choose a date in the future',
+        messageColor: 'red',
+        messageSize: 25,
+        timeout: 2000,
+        position: 'center',
+      });
+
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+const fp = flatpickr(inputText, options);
+
+function startTimer() {
+  let restTime = userSelectedDate.getTime() - new Date().getTime();
+
+  const timerInterval = setInterval(updateTimer, 1000);
+
+  function updateTimer() {
+    restTime -= 1000; // Subtract 1 second
+
+    if (restTime <= 0) {
+      clearInterval(timerInterval);
+      restTime = 0;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(restTime);
+
+    timerDays.textContent = addLeadingZero(days);
+    timerHours.textContent = addLeadingZero(hours);
+    timerMinutes.textContent = addLeadingZero(minutes);
+    timerSeconds.textContent = addLeadingZero(seconds);
+  }
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+startButton.addEventListener('click', startTimer);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
